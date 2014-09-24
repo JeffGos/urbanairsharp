b59f30fdd3e3395a85ed325da8e2e33fbcb2d408
Andy Long [work] <andy@theouteredge.co.uk>
2014-09-24 15:20:33 +0100

    Added the abilit to register a device token with Urban Airship
    This can be useful for new devices and when a device token becomes inactive within UA system.
    
    Created an new DeviceToken class, PutRequest and DeviceTokenRequest and extended the UrbanAirSharpGatway to expose this in a simple method call.
    
    Also added a test in the TestApp so you can register a device token

--- src/UrbanAirSharp/UrbanAirSharpGateway.cs
+++ src/UrbanAirSharp/UrbanAirSharpGateway.cs
@@ -19,170 +19,181 @@ using UrbanAirSharp.Type;
 
 namespace UrbanAirSharp
 {
-	/// <summary>
-	/// A gateway for pushing notifications to the Urban Airship API V3
-	/// http://docs.urbanairship.com/reference/api/v3/
-	/// 
-	/// Supported:
-	/// ---------
-	/// api/push
-	/// api/push/validate
-	/// api/schedule 
-	/// 
-	/// Not Supported Yet:
-	/// -----------------
-	/// api/tags 
-	/// api/feeds 
-	/// api/reports 
-	/// api/device_tokens 
-	/// api/segments 
-	/// api/location 
-	/// </summary>
+    /// <summary>
+    /// A gateway for pushing notifications to the Urban Airship API V3
+    /// http://docs.urbanairship.com/reference/api/v3/
+    /// 
+    /// Supported:
+    /// ---------
+    /// api/push
+    /// api/push/validate
+    /// api/schedule 
+    /// 
+    /// Not Supported Yet:
+    /// -----------------
+    /// api/tags 
+    /// api/feeds 
+    /// api/reports 
+    /// api/device_tokens 
+    /// api/segments 
+    /// api/location 
+    /// </summary>
 
-	public class UrbanAirSharpGateway
-	{
-		private static readonly ILog Log = LogManager.GetLogger(typeof(UrbanAirSharpGateway));
+    public class UrbanAirSharpGateway
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(UrbanAirSharpGateway));
 
-		public UrbanAirSharpGateway(String appKey, String appMasterSecret)
-		{
-			XmlConfigurator.Configure();
-			ServiceModelConfig.Create(appKey, appMasterSecret);
-		}
+        public UrbanAirSharpGateway(String appKey, String appMasterSecret)
+        {
+            XmlConfigurator.Configure();
+            ServiceModelConfig.Create(appKey, appMasterSecret);
+        }
 
-		/// <summary>
-		/// - Broadcast to all devices
-		/// - Broadcast to one device type
-		/// - Send to a targeted device
-		/// - Broadcast to all devices with a different alert for each type
-		/// </summary>
-		/// <param name="alert">The message to be pushed</param>
-		/// <param name="deviceTypes">use null for broadcast</param>
-		/// <param name="deviceId">use null for broadcast or deviceTypes must contain 1 element that distinguishes this deviceId</param>
-		/// <param name="deviceAlerts">per device alert messages and extras</param>
-		/// <param name="customAudience">a more specific way to choose the audience for the push. If this is set, deviceId is ignored</param>
-		/// <returns></returns>
-		public BaseResponse Push(String alert, IList<DeviceType> deviceTypes = null, String deviceId = null, IList<BaseAlert> deviceAlerts = null, Audience customAudience = null)
-		{
-			var request = new PushRequest(CreatePush(alert, deviceTypes, deviceId, deviceAlerts, customAudience));
-			
-			var response = request.ExecuteAsync();
+        /// <summary>
+        /// - Broadcast to all devices
+        /// - Broadcast to one device type
+        /// - Send to a targeted device
+        /// - Broadcast to all devices with a different alert for each type
+        /// </summary>
+        /// <param name="alert">The message to be pushed</param>
+        /// <param name="deviceTypes">use null for broadcast</param>
+        /// <param name="deviceId">use null for broadcast or deviceTypes must contain 1 element that distinguishes this deviceId</param>
+        /// <param name="deviceAlerts">per device alert messages and extras</param>
+        /// <param name="customAudience">a more specific way to choose the audience for the push. If this is set, deviceId is ignored</param>
+        /// <returns></returns>
+        public BaseResponse Push(String alert, IList<DeviceType> deviceTypes = null, String deviceId = null, IList<BaseAlert> deviceAlerts = null, Audience customAudience = null)
+        {
+            var request = new PushRequest(CreatePush(alert, deviceTypes, deviceId, deviceAlerts, customAudience));
+            
+            var response = request.ExecuteAsync();
 
-			return response.Result;
-		}
+            return response.Result;
+        }
 
-		/// <summary>
-		/// Validates a push request. Duplicates Push without actually sending the alert. See Push
-		/// </summary>
-		/// <param name="alert">The message to be pushed</param>
-		/// <param name="deviceTypes">use null for broadcast</param>
-		/// <param name="deviceId">use null for broadcast or deviceTypes must contain 1 element that distinguishes this deviceId</param>
-		/// <param name="deviceAlerts">per device alert messages and extras</param>
-		/// <param name="customAudience">a more specific way to choose the audience for the push. If this is set, deviceId is ignored</param>
-		/// <returns></returns>
-		public BaseResponse Validate(String alert, IList<DeviceType> deviceTypes = null, String deviceId = null,
-			IList<BaseAlert> deviceAlerts = null, Audience customAudience = null)
-		{
-			var request = new PushValidateRequest(CreatePush(alert, deviceTypes, deviceId, deviceAlerts, customAudience));
+        /// <summary>
+        /// Validates a push request. Duplicates Push without actually sending the alert. See Push
+        /// </summary>
+        /// <param name="alert">The message to be pushed</param>
+        /// <param name="deviceTypes">use null for broadcast</param>
+        /// <param name="deviceId">use null for broadcast or deviceTypes must contain 1 element that distinguishes this deviceId</param>
+        /// <param name="deviceAlerts">per device alert messages and extras</param>
+        /// <param name="customAudience">a more specific way to choose the audience for the push. If this is set, deviceId is ignored</param>
+        /// <returns></returns>
+        public BaseResponse Validate(String alert, IList<DeviceType> deviceTypes = null, String deviceId = null,
+            IList<BaseAlert> deviceAlerts = null, Audience customAudience = null)
+        {
+            var request = new PushValidateRequest(CreatePush(alert, deviceTypes, deviceId, deviceAlerts, customAudience));
 
-			var response = request.ExecuteAsync();
+            var response = request.ExecuteAsync();
 
-			return response.Result;
-		}
+            return response.Result;
+        }
 
-		public BaseResponse ScheduleAdd(String alert, DateTime triggerDate)
-		{
-			//TODO:
-			return null;
-		}
+        public BaseResponse ScheduleAdd(String alert, DateTime triggerDate)
+        {
+            //TODO:
+            return null;
+        }
 
-		public BaseResponse ScheduleEdit(Guid scheduleId, String alert, DateTime triggerDate)
-		{
-			//TODO:
-			return null;
-		}
+        public BaseResponse ScheduleEdit(Guid scheduleId, String alert, DateTime triggerDate)
+        {
+            //TODO:
+            return null;
+        }
 
-		public BaseResponse ScheduleDelete(Guid scheduleId)
-		{
-			//TODO:
-			return null;
-		}
+        public BaseResponse ScheduleDelete(Guid scheduleId)
+        {
+            //TODO:
+            return null;
+        }
 
-		public BaseResponse ScheduleGet(Guid scheduleId)
-		{
-			//TODO:
-			return null;
-		}
+        public BaseResponse ScheduleGet(Guid scheduleId)
+        {
+            //TODO:
+            return null;
+        }
 
-		public BaseResponse SchedulesList()
-		{
-			//TODO:
-			return null;
-		}
+        public BaseResponse SchedulesList()
+        {
+            //TODO:
+            return null;
+        }
 
-		private static Push CreatePush(String alert, IList<DeviceType> deviceTypes = null, String deviceId = null, IList<BaseAlert> deviceAlerts = null, Audience customAudience = null)
-		{
-			var push = new Push()
-			{
-				Notification = new Notification()
-				{
-					DefaultAlert = alert
-				}
-			};
+        private static Push CreatePush(String alert, IList<DeviceType> deviceTypes = null, String deviceId = null, IList<BaseAlert> deviceAlerts = null, Audience customAudience = null)
+        {
+            var push = new Push()
+            {
+                Notification = new Notification()
+                {
+                    DefaultAlert = alert
+                }
+            };
 
-			if (customAudience != null)
-			{
-				deviceId = null;
+            if (customAudience != null)
+            {
+                deviceId = null;
 
-				push.Audience = customAudience;
-			}
+                push.Audience = customAudience;
+            }
 
-			if (deviceTypes != null)
-			{
-				push.DeviceTypes = deviceTypes;
+            if (deviceTypes != null)
+            {
+                push.DeviceTypes = deviceTypes;
 
-				if (deviceId != null)
-				{
-					if (deviceTypes.Count != 1)
-					{
-						throw new InvalidOperationException("when deviceId is not null, deviceTypes must contain 1 element which identifies the deviceId type");
-					}
+                if (deviceId != null)
+                {
+                    if (deviceTypes.Count != 1)
+                    {
+                        throw new InvalidOperationException("when deviceId is not null, deviceTypes must contain 1 element which identifies the deviceId type");
+                    }
 
-					var deviceType = deviceTypes[0];
+                    var deviceType = deviceTypes[0];
 
-					switch (deviceType)
-					{
-						case DeviceType.Android:
-							push.SetAudience(AudienceType.Android, deviceId);
-							break;
-						case DeviceType.Ios:
-							push.SetAudience(AudienceType.Ios, deviceId);
-							break;
-						case DeviceType.Wns:
-							push.SetAudience(AudienceType.Windows, deviceId);
-							break;
-						case DeviceType.Mpns:
-							push.SetAudience(AudienceType.WindowsPhone, deviceId);
-							break;
-						case DeviceType.Blackberry:
-							push.SetAudience(AudienceType.Blackberry, deviceId);
-							break;
-					}
-				}
-			}
+                    switch (deviceType)
+                    {
+                        case DeviceType.Android:
+                            push.SetAudience(AudienceType.Android, deviceId);
+                            break;
+                        case DeviceType.Ios:
+                            push.SetAudience(AudienceType.Ios, deviceId);
+                            break;
+                        case DeviceType.Wns:
+                            push.SetAudience(AudienceType.Windows, deviceId);
+                            break;
+                        case DeviceType.Mpns:
+                            push.SetAudience(AudienceType.WindowsPhone, deviceId);
+                            break;
+                        case DeviceType.Blackberry:
+                            push.SetAudience(AudienceType.Blackberry, deviceId);
+                            break;
+                    }
+                }
+            }
 
-			if (deviceAlerts == null || deviceAlerts.Count <= 0)
-			{
-				return push;
-			}
+            if (deviceAlerts == null || deviceAlerts.Count <= 0)
+            {
+                return push;
+            }
 
-			push.Notification.AndroidAlert = (AndroidAlert)deviceAlerts.FirstOrDefault(x => x is AndroidAlert);
-			push.Notification.IosAlert = (IosAlert)deviceAlerts.FirstOrDefault(x => x is IosAlert);
-			push.Notification.WindowsAlert = (WindowsAlert)deviceAlerts.FirstOrDefault(x => x is WindowsAlert);
-			push.Notification.WindowsPhoneAlert = (WindowsPhoneAlert)deviceAlerts.FirstOrDefault(x => x is WindowsPhoneAlert);
-			push.Notification.BlackberryAlert = (BlackberryAlert)deviceAlerts.FirstOrDefault(x => x is BlackberryAlert);
+            push.Notification.AndroidAlert = (AndroidAlert)deviceAlerts.FirstOrDefault(x => x is AndroidAlert);
+            push.Notification.IosAlert = (IosAlert)deviceAlerts.FirstOrDefault(x => x is IosAlert);
+            push.Notification.WindowsAlert = (WindowsAlert)deviceAlerts.FirstOrDefault(x => x is WindowsAlert);
+            push.Notification.WindowsPhoneAlert = (WindowsPhoneAlert)deviceAlerts.FirstOrDefault(x => x is WindowsPhoneAlert);
+            push.Notification.BlackberryAlert = (BlackberryAlert)deviceAlerts.FirstOrDefault(x => x is BlackberryAlert);
 
-			return push;
-		}
+            return push;
+        }
 
-	}
+
+        public BaseResponse RegisterDeviceToken(string deviceToken)
+        {
+            if (string.IsNullOrEmpty(deviceToken))
+                throw new ArgumentException("A device Token is Required", "deviceToken");
+
+            var deviceRequest = new DeviceTokenRequest(new DeviceToken() {Token = deviceToken});
+            var requestTask = deviceRequest.ExecuteAsync();
+
+            return requestTask.Result;
+        }
+    }
 }
